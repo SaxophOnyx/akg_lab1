6b4fc612c2fd767dfa6b662d182841bc1abd0dcd
bulbashist <nbulbashist@gmail.com>
2023-10-14 12:51:21 +0300

    alive cat v1

--- GraphicsLabSFML/Program.cs
+++ GraphicsLabSFML/Program.cs
@@ -16,11 +16,16 @@ namespace GraphicsLabSFML
         private static float _angleX = 0;
         private static float _angleY = 0;
         private static float _angleZ = 0;
-        private static Vector3 _cameraPos = new(-250, -250, -1250);
-        //private static Vector3 _modelPos = new(width / 2, height / 2, 100);
-        private static Vector3 _modelPos = new(100, 100, 100);
+        private static Vector3 _cameraPos = new(0, 0, 0);
+        private static Vector3 _modelPos = new(0, 0, -10);
 
         private static float _scale = 1;
+        private static Vector3 _position = new(0);
+
+        private static Matrix4x4 viewportM;
+        private static Matrix4x4 projectionM;
+        private static Matrix4x4 viewM;
+        private static Matrix4x4 modelM;
 
         static void Main()
         {
@@ -54,18 +59,9 @@ namespace GraphicsLabSFML
 
         private static Matrix4x4 createViewMatrix(Vector3 eye, Vector3 target, Vector3 up)
         {
-
-            Vector3 zAxis1 = eye - target;
-            Vector3 xAxis1 = up * zAxis1;
-            Vector3 yAxis1 = zAxis1 * xAxis1;
-
-        //    Vector3 zAxis = Vector3.Normalize(zAxis1);
-      //      Vector3 xAxis = Vector3.Normalize(yAxis1);
-      //      Vector3 yAxis = Vector3.Normalize(xAxis1);
-
-                   Vector3 zAxis = Vector3.Normalize(eye - target);
-                   Vector3 xAxis = Vector3.Normalize(up * zAxis);
-                   Vector3 yAxis = up;
+            Vector3 zAxis = Vector3.Normalize(eye - target);
+            Vector3 xAxis = Vector3.Normalize(Vector3.Cross(up, zAxis));
+            Vector3 yAxis = up;
 
             return new Matrix4x4(
               xAxis.X, xAxis.Y, xAxis.Z, -Vector3.Dot(xAxis, eye),
@@ -73,6 +69,17 @@ namespace GraphicsLabSFML
               zAxis.X, zAxis.Y, zAxis.Z, -Vector3.Dot(zAxis, eye),
               0, 0, 0, 1
             );
+        }
+
+        private static Matrix4x4 createProjectionMatrix(float znear, float zfar) {
+            var a = 2 * 10 / 1000;
+            var b = 2 * 10f / 1000;
+            return new Matrix4x4(
+                2f / width, 0, 0, 0,
+                0, 2f / height, 0, 0,
+                0, 0, 1 / (znear - zfar), znear / (znear - zfar),
+                0, 0, 0, 1
+                );
         }
 
         private static Matrix4x4 CreateResultMatrix(uint width, uint height)
@@ -83,45 +90,44 @@ namespace GraphicsLabSFML
             Matrix4x4 scale = Matrix4x4.CreateScale(_scale);
             // Matrix4x4 offset = Matrix4x4.CreateTranslation(width / 2, height / 2, 0);
             Matrix4x4 offset = Matrix4x4.CreateTranslation(new Vector3(width / 2,  height / 2, 10));
+            Matrix4x4 offset2 = Matrix4x4.CreateTranslation(_position);
             Matrix4x4 rotateX = Matrix4x4.CreateRotationX(Utils.DegreesToRadians(_angleX));
             Matrix4x4 rotateY = Matrix4x4.CreateRotationY(Utils.DegreesToRadians(_angleY));
             Matrix4x4 rotateZ = Matrix4x4.CreateRotationZ(Utils.DegreesToRadians(_angleZ));
 
-            Matrix4x4 camera = Matrix4x4.CreateLookAt(_cameraPos, new Vector3(), Vector3.UnitY);
-            Matrix4x4 projection = Matrix4x4.CreatePerspective(width, height, 1f, 10000f);
+            //same one
+            Matrix4x4 camera = Matrix4x4.CreateLookAt(_cameraPos, _modelPos, new Vector3(0, 1, 0));
+
+            Matrix4x4 projection = 
+                createProjectionMatrix(0.01f, 1);
+
+                //not works;
+                //Matrix4x4.CreatePerspective(width, height, 0.01f, 1f);
             Matrix4x4 screen = Matrix4x4Factories.CreateScreen(1280, 720);
 
-            Vector3 eye = new(0, 0, 0);
-            Vector3 target = _modelPos;             //new Vector3(0, 0, 0);
-            Vector3 up = new(0, 1000, 0);
+            Vector3 eye = _cameraPos;
+            Vector3 target = _modelPos;       
+            Vector3 up = new(0, 1, 0);
 
-            Matrix4x4 viewport1 = screen;
-            Matrix4x4 projection1 = Matrix4x4.Transpose(projection);
-            Matrix4x4 view1 = createViewMatrix(eye, target, up);
-            //Matrix4x4 view1 = Matrix4x4.Transpose(Matrix4x4.CreateBillboard(target, eye, up, new Vector3(0,0,0)));
-            Matrix4x4 model1 = scale * rotateZ * rotateY * rotateX * offset;
+            viewportM = Matrix4x4.Transpose(screen);
+            projectionM = Matrix4x4.Transpose(projection);
+            viewM = Matrix4x4.Transpose(createViewMatrix(eye, target, up));
+            modelM = scale * rotateZ * rotateY * rotateX * offset * offset2;
 
-            // Matrix4x4 res = scale * rotateZ * rotateY * rotateX * offset * projection;
             Matrix4x4 res =
-                viewport1 *
-                projection1 *
-                view1 * 
-                model1;
+                viewportM *
+                projectionM *
+                viewM *
+                modelM;
 
             return res;
         }
     
         private static IEnumerable<Vector4> TransformVertices(IEnumerable<Vector4> vertices, Matrix4x4 matrix)
         {
-               var a = vertices.Select(v => Vector4.Transform(v, matrix)).Select(v => v / v.W);
-            //var a = vertices.Select(v => v / v.W);
+            var a = vertices.Select(v => Vector4.Transform(v, matrix));//.Select(v => v / v.W);
+           // var a = vertices.Select(v => v / v.W);
             return a;
-
-            var v1 = vertices.Select(v => Vector4.Transform(v, matrix));
-            var v2 = v1.Select(v => v / v.W);
-            var v3 = v2.Where(v => (v.X > 0 && v.X < 1) && (v.Y > 0 && v.Y < 1));
-            
-            return v3;
         }
 
         private static void OnKeyPressed(object? sender, KeyEventArgs e)
@@ -135,45 +141,49 @@ namespace GraphicsLabSFML
                 // Model rotation
                 case Keyboard.Key.W:
                 {
-                    _angleX += rotateDelta;
-                    break;
+                        // _angleX += rotateDelta;
+                        _cameraPos.X += rotateDelta;
+                        break;
                 }
                 case Keyboard.Key.S:
                 {
-                    _angleX -= rotateDelta;
-                    break;
+                 //   _angleX -= rotateDelta;
+                        _cameraPos.Y -= rotateDelta;
+                        break;
                 }
                 case Keyboard.Key.A:
                 {
-                    _angleY -= rotateDelta;
+                        // _angleY -= rotateDelta;
+                        _cameraPos.X -= rotateDelta;
                     break;
                 }
                 case Keyboard.Key.D:
                 {
-                    _angleY += rotateDelta;
-                    break;
+                  //  _angleY += rotateDelta;
+                        _cameraPos.X += rotateDelta;
+                        break;
                 }
                 case Keyboard.Key.E:
                 {
-                    _angleZ += rotateDelta;
-                    break;
+                  //  _angleZ += rotateDelta;
+                        _cameraPos.Z += rotateDelta;
+                        break;
                 }
                 case Keyboard.Key.Q:
                 {
-                    _angleZ -= rotateDelta;
-                    break;
+                    //_angleZ -= rotateDelta;
+                        _cameraPos.Z -= rotateDelta;
+                        break;
                 }
                 case Keyboard.Key.Equal:
                 {
-                    _scale += scaleDelta;
+                    //_scale += scaleDelta;
+                    _scale *= 1.5f;
                     break;
                 }
                 case Keyboard.Key.Hyphen:
                 {
-                    if (_scale > 1)
-                    {
-                        _scale -= scaleDelta;
-                    }
+                        _scale *= 0.7f;
 
                     break;
                 }
@@ -181,22 +191,23 @@ namespace GraphicsLabSFML
                 // Model movement
                 case Keyboard.Key.Up:
                 {
-                    _modelPos.Z += movementDelta;
+                    _position.Y += movementDelta;
                     break;
                 }
                 case Keyboard.Key.Down:
                 {
-                    _modelPos.Z -= movementDelta;
+                    _position.Y -= movementDelta;
                     break;
                 }
                 case Keyboard.Key.Left:
                 {
-                    _modelPos.X -= movementDelta;
+                    _position.X -= movementDelta;
                     break;
                 }
                 case Keyboard.Key.Right:
                 {
-                    _modelPos.X += movementDelta;
+                        _position.X += movementDelta;
+                  //  _modelPos.X += movementDelta;
                     break;
                 }
                 case Keyboard.Key.Space:
